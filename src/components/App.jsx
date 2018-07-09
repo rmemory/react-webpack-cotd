9a4c32148ab2ef94925e5fc39d01c783b3adc994
Richard Memory <richardfmemory@gmail.com>
2018-07-08 20:51:13 -0600

    Add initial Header, Inventory, and Order

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
+import Header from './Header.jsx';
+import Order from './Order.jsx';
+import Inventory from './Inventory.jsx';
+
 class App extends Component {
 	render() {
 		return (
 			<div className="catch-of-the-day">
-				<p>stuff</p>
+				<div className="menu">
+					<Header tagline="Fresh Seafood Market" />
+				</div>
+				<Order />
+				<Inventory />
 			</div>
 		);
 	}
---
+++ src/components/Inventory.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Inventory extends Component {
+	render() {
+		return (
+			<div className="inventory">
+				<h1>Inventory</h1>
+			</div>
+		);
+	}
+}
+
+export default Inventory;
